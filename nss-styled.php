ade899c33689bb05ff81bb175d6eb2ae8b850b8a
Ashley Evans <cazzyae@gmail.com>
2016-10-27 13:23:12 +0100

    Initial commit.

---
+++ nss-styled.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: NSS - Styled Buttons
+ * Plugin URI: https://www.nosegraze.com
+ * Description: Styled Naked Social Share buttons.
+ * Version: 1.0
+ * Author: Nose Graze
+ * Author URI: https://www.nosegraze.com
+ * License: GPL2
+ * 
+ * @package nss-styled
+ * @copyright Copyright (c) 2016, Nose Graze Ltd.
+ * @license GPL2+
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
